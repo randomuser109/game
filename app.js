4bd1251c14bbc265ae33f0e08e41928969593681
randomuser109 <yuobutechannel53@gmail.com.noreply.github.com>
2023-06-06 12:02:14 +0200

    index.html

--- app.js
+++ app.js
@@ -1,18 +1,32 @@
-var h;
-
+let ComputerChoice2;
 function GetComputerChoice() {
   let ComputerChoice = ["rock", "paper", "scissors"];
 
   let ComputerValue =
     ComputerChoice[Math.floor(Math.random() * ComputerChoice.length)];
-  h = ComputerValue;
-  console.log(h);
+  ComputerChoice2 = ComputerValue;
 }
 
-function a() {
-  let player = document.getElementById("prock");
+function rock() {
+  let player = document.getElementById("rock");
   GetComputerChoice();
-  if (player.innerText === "rock" && h === "paper") {
-    console.log("A");
-  } else console.log("n");
+  if (player.innerText === "rock" && ComputerChoice2 === "scissors") {
+    console.log("rocks cruhes scissors player win");
+  } else console.log("paper cover rock computer ");
+}
+
+function paper() {
+  let player = document.getElementById("paper");
+  GetComputerChoice();
+  if (player.innerText === "paper" && ComputerChoice2 === "rock") {
+    console.log("paper cover rock player win");
+  } else console.log("scissors  cuts paper computer ");
+}
+
+function scissors() {
+  let player = document.getElementById("scissors");
+  GetComputerChoice();
+  if (player.innerText === "scissors" && ComputerChoice2 === "paper") {
+    console.log("scissors  cuts paper player win");
+  } else console.log("rocks crushes scissors computer ");
 }
